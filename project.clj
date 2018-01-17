81f861f07f098f72a984129a3cbf765be971be3a
Dragan Djuric <dragandj@gmail.com>
2018-01-17 13:40:46 +0100

    Release 0.3.0.

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
 ;;   the terms of this license.
 ;;   You must not remove this notice, or any other, from this software.
 
-(defproject uncomplicate/clojurecuda "0.3.0-SNAPSHOT"
+(defproject uncomplicate/clojurecuda "0.3.0"
   :description "ClojureCUDA is a Clojure library for parallel computations with Nvidia's CUDA."
   :url "https://github.com/uncomplicate/clojurecuda"
   :scm {:name "git"
@@ -15,9 +15,9 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.jcuda/jcuda "0.9.0"]
-                 [org.clojure/core.async "0.3.465"]
-                 [uncomplicate/commons "0.4.0-SNAPSHOT"]
-                 [uncomplicate/fluokitten "0.6.1-SNAPSHOT"]]
+                 [org.clojure/core.async "0.4.474"]
+                 [uncomplicate/commons "0.4.0"]
+                 [uncomplicate/fluokitten "0.6.1"]]
 
   :codox {:metadata {:doc/format :markdown}
           :src-dir-uri "http://github.com/uncomplicate/clojurecuda/blob/master/"
