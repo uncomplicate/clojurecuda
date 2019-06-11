c9a0b3da9456cd7a6a01b49bb42228a860f1ddb0
Dragan Djuric <dragandj@gmail.com>
2019-06-11 21:10:04 +0200

    Upgrade commons.

--- README.md
+++ README.md
@@ -6,6 +6,6 @@ Clojure library for CUDA development. See the documentation at [ClojureCUDA webs
 
 ## License
 
-Copyright © 2017-2018 Dragan Djuric
+Copyright © 2017-2019 Dragan Djuric
 
 Distributed under the Eclipse Public License either version 1.0 or (at your option) any later version.
--- project.clj
+++ project.clj
@@ -6,17 +6,17 @@
 ;;   the terms of this license.
 ;;   You must not remove this notice, or any other, from this software.
 
-(defproject uncomplicate/clojurecuda "0.7.0"
+(defproject uncomplicate/clojurecuda "0.7.1-SNAPSHOT"
   :description "ClojureCUDA is a Clojure library for parallel computations with Nvidia's CUDA."
   :url "https://github.com/uncomplicate/clojurecuda"
   :scm {:name "git"
         :url "https://github.com/uncomplicate/clojurecuda"}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.jcuda/jcuda "10.1.0"]
                  [org.clojure/core.async "0.4.490"]
-                 [uncomplicate/commons "0.7.2"]
+                 [uncomplicate/commons "0.8.0-SNAPSHOT"]
                  [uncomplicate/fluokitten "0.9.1"]]
 
   :codox {:metadata {:doc/format :markdown}
@@ -31,7 +31,7 @@
                        uncomplicate.clojurecuda.internal.utils]}
 
   :profiles {:dev {:plugins [[lein-midje "3.2.1"]
-                             [lein-codox "0.10.3"]]
+                             [lein-codox "0.10.6"]]
                    :global-vars {*warn-on-reflection* true
                                  *assert* true
                                  *unchecked-math* :warn-on-boxed
