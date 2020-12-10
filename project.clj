532ef26e0467d7963a37d7806634125ce091728b
Dragan Djuric <dragandj@gmail.com>
2020-12-10 17:24:36 +0100

    Release 0.12.0.

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
 ;;   the terms of this license.
 ;;   You must not remove this notice, or any other, from this software.
 
-(defproject uncomplicate/clojurecuda "0.11.0"
+(defproject uncomplicate/clojurecuda "0.12.0"
   :description "ClojureCUDA is a Clojure library for parallel computations with Nvidia's CUDA."
   :url "https://github.com/uncomplicate/clojurecuda"
   :scm {:name "git"
@@ -14,7 +14,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.jcuda/jcuda "11.0.0"]
+                 [org.jcuda/jcuda "11.1.1"]
                  [org.clojure/core.async "1.3.610"]
                  [uncomplicate/commons "0.12.0"]
                  [uncomplicate/fluokitten "0.9.1"]]
