6d06499baf49af75031a0692462674031eb2c299
Dragan Djuric <dragandj@gmail.com>
2022-06-29 12:06:14 +0200

    Release 0.15.1.

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
 ;;   the terms of this license.
 ;;   You must not remove this notice, or any other, from this software.
 
-(defproject uncomplicate/clojurecuda "0.15.1-SNAPSHOT"
+(defproject uncomplicate/clojurecuda "0.15.1"
   :description "ClojureCUDA is a Clojure library for parallel computations with Nvidia's CUDA."
   :url "https://github.com/uncomplicate/clojurecuda"
   :scm {:name "git"
