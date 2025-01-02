c668f97bccebd5798c80ed954e422cccc80766bb
Dragan Djuric <dragandj@gmail.com>
2025-01-02 21:28:14 +0100

    Update to JavaCPP 1.5.11 and cuda 12.6.

--- project.clj
+++ project.clj
@@ -6,19 +6,19 @@
 ;;   the terms of this license.
 ;;   You must not remove this notice, or any other, from this software.
 
-(defproject uncomplicate/clojurecuda "0.19.0"
+(defproject uncomplicate/clojurecuda "0.20.0-SNAPSHOT"
   :description "ClojureCUDA is a Clojure library for parallel computations with Nvidia's CUDA."
   :url "https://github.com/uncomplicate/clojurecuda"
   :scm {:name "git"
         :url "https://github.com/uncomplicate/clojurecuda"}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.11.3"]
-                 [org.clojure/core.async "1.6.681"]
-                 [uncomplicate/commons "0.15.0"]
+  :dependencies [[org.clojure/clojure "1.12.0"]
+                 [org.clojure/core.async "1.7.701"]
+                 [uncomplicate/commons "0.15.1"]
                  [uncomplicate/fluokitten "0.10.0"]
-                 [org.uncomplicate/clojure-cpp "0.3.0"]
-                 [org.bytedeco/cuda-platform "12.3-8.9-1.5.10"]]
+                 [org.uncomplicate/clojure-cpp "0.4.0"]
+                 [org.bytedeco/cuda-platform "12.6-9.5-1.5.11"]]
 
   :profiles {:dev {:plugins [[lein-midje "3.2.1"]
                              [lein-codox "0.10.8"]
@@ -29,7 +29,7 @@
                                  *print-length* 128}
                    :dependencies [[midje "1.10.10"]
                                   [codox-theme-rdash "0.1.2"]
-                                  [org.bytedeco/cuda "12.3-8.9-1.5.10" :classifier linux-x86_64-redist]]
+                                  [org.bytedeco/cuda "12.6-9.5-1.5.11" :classifier linux-x86_64-redist]]
                    :codox {:metadata {:doc/format :markdown}
                            :source-uri "http://github.com/uncomplicate/clojurecuda/blob/master/{filepath}#L{line}"
                            :output-path "docs/codox"
